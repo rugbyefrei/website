9b4e6136237b11e8e574f405f6f3fc8a0460cf17
Jérémie <barriere.jeremie@gmail.com>
2023-11-06 14:15:00 +0100

    [fix] text center on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center  bg-[#045700]">
-      <h1 className={"text-white uppercase text-7xl font-heartOne tracking-widest"}>EFREI Rugby</h1>
+      <h1 className={"text-white text-center uppercase text-7xl font-heartOne tracking-widest"}>EFREI Rugby</h1>
     </main>
   )
 }
