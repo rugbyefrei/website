70d57eed30458a175fab3eaf46b903ccbd0d25e0
Jérémie <barriere.jeremie@gmail.com>
2023-11-06 13:26:37 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center  bg-[#045700]">
+      <h1 className={"text-white uppercase text-7xl font-heartOne tracking-widest"}>EFREI Rugby</h1>
+    </main>
+  )
+}
